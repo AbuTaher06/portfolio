5893d573301aadaf938c78f3385e11ebdd4554af
Md Abu Taher <83139817+AbuTaher06@users.noreply.github.com>
2024-08-25 01:23:30 +0600

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,14 @@ import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Projects from '../views/Projects.vue';
 import Contact from '../views/Contact.vue';
-//import ProjectDetail from '../views/ProjectDetail.vue'; // Ensure this path is correct
+import ProjectDetail from '../views/ProjectDetail.vue'; // Ensure this path is correct
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/about', name: 'About', component: About },
   { path: '/projects', name: 'Projects', component: Projects },
   { path: '/contact', name: 'Contact', component: Contact },
-//  { path: '/project/:id', name: 'ProjectDetail', component: ProjectDetail },
+  { path: '/project/:id', name: 'ProjectDetail', component: ProjectDetail },
 ];
 
 const router = createRouter({
